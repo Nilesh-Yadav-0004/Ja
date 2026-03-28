5737c1f64e0f9c8a8dcbe964f8c35888768e508a
Nilesh Yadav <ny033222@gmail.com>
2026-03-28 19:38:46 +0530

    code of the day

--- JavaScript/Datatypes.js
+++ JavaScript/Datatypes.js
@@ -1,7 +1,12 @@
 // Number 
-let age =  21;
-console.log(age)
-let price = 99.99;
-console.log(price)
+// let age =  21;
+// console.log(age)
+// let price = 99.99;
+// console.log(price)
 
 //  String
+let name ="Nilesh";
+console.log(name)
+let msg = 'Hello Nilesh';
+console.log(msg)
+
