08afd7dd9479fd4d2a68ed670d4b5f91c3f4be39
Nilesh Yadav <ny033222@gmail.com>
2026-03-29 12:10:53 +0530

    Code

--- JavaScript/Datatypes.js
+++ JavaScript/Datatypes.js
@@ -5,8 +5,11 @@
 // console.log(price)
 
 //  String
-let name ="Nilesh";
-console.log(name)
-let msg = 'Hello Nilesh';
-console.log(msg)
+// let name ="Nilesh";
+// console.log(name)
+// let msg = 'Hello Nilesh';
+// console.log(msg)
 
+// Boolean 
+let isLoggedIn = true;
+console.log(isLoggedIn)
