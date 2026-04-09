5a31028b4673fb4dd70b97934feb413f5e1c72cd
Nilesh Yadav <ny033222@gmail.com>
2026-04-09 18:12:52 +0530

    code of the day

---
+++ JavaScript/Switch.js
@@ -0,0 +1,22 @@
+const option = 4;
+
+const number_1 = 2;
+const number_2 = 5;
+
+switch (option) {
+    case 1:
+        console.log(number_1 + number_2, 'Addition');
+        break;
+    case 2:
+        console.log(number_1 - number_2, 'subtraction');
+        break;
+    case 3:
+        console.log(number_1 * number_2, 'Multiplication');
+        break;
+    case 4:
+        console.log(number_1 / number_2, 'Division');
+        break;
+
+    default:
+        console.log(' please enter the value between 1 to 4 ');
+}
